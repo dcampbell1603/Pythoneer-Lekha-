721bf8f20180ac8aef597908f620ab6f23a8bcfd
Dylan Campbell <dcampbell1603@gmail.com>
2025-03-28 22:06:21 -0500

    added new pygame font renderer, cleans up text

--- Flappy-Bird Game/flappy.py
+++ Flappy-Bird Game/flappy.py
@@ -1,6 +1,7 @@
 import random  # For generating random numbers
 import sys  # We will use sys.exit to exit the program
 import pygame
+import pygame.freetype
 from pygame.locals import *  # Basic pygame imports
 
 # Global Variables for the game
